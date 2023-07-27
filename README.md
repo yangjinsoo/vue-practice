dde7fe4ed093b0c8709a54bd97ddf9b12618c7c0
yangjinsoo <wlstn0418@gmail.com>
2023-07-27 22:40:19 +0900

    if,for,데이터바인딩

--- README.md
+++ README.md
@@ -26,5 +26,21 @@ npm run build
 npm run lint
 ```
 
+### 메모장
+```vue
+v-for="arr in arrs" :key="arr"
+v-for="(arr, i) in arrs" :key="i"
+```
+- v-for을 사용할 때는 :key가 꼭 필요
+- (arr, i)를 통해 python의 enumerate처럼 인덱스도 함께 받아올 수 있음.
+***
+- 동적 UI 만드는 순서
+    - UI를 미리 만들어놓음.
+    - UI의 상태를 데이터로 저장해놓음.
+    - UI의 상태 변화에 따라 UI가 어떻게 보일지 작성.
+
+- html태그 사이에 데이터 사용하려면 {{데이터}}
+- html태그 속성에 데이터 사용하려면 :속성명="데이터"
+
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,105 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <div v-if="모달창열렸나==true" class="black-bg">
+    <div class="white-bg">
+      <h4>{{products[product_num].title}}</h4>
+      <p>{{products[product_num].content}}</p>
+      <button @click="모달창열렸나=false" class="modal-btn">닫기</button>
+    </div>
+  </div>
+
+  <div class="menu">
+    <a href="#">Home</a>
+    <a href="#">Products</a>
+    <a href="#">About</a>
+  </div>
+  <div v-for="(product, i) in products" :key="i">
+    <img class="room-img" :src="product.image" alt="매물사진">
+    <h4 id="product-name" @click="openModal(i)">{{product.title}}</h4>
+    <p>{{product.price}} 원</p>
+  </div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import data_products from "./data_product.js";
 
 export default {
   name: 'App',
+  data() {
+    return {
+      product_num : 0,
+      products : data_products,
+      모달창열렸나 : false,
+    }
+  },
+  methods: {
+    openModal(i) {
+      this.product_num = i;
+      this.모달창열렸나 = true;
+    }
+  },
   components: {
-    HelloWorld
+
   }
 }
 </script>
 
 <style>
+.body {
+  margin: 0;
+}
+
+div {
+  box-sizing: border-box;
+}
+
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background: rgba(0,0,0,0.5);
+  position: fixed;
+  padding: 20px;
+}
+
+.white-bg {
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+
+.room-img {
+  width: 100%;
+  margin-top: 40px;
+}
+
+.modal-btn {
+  float: right;
+  cursor: pointer;
+  margin-bottom: 10px;
+}
+
+#product-name {
+  cursor: pointer;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+
+.menu a {
+  color: white;
+  padding: 10px;
 }
 </style>
